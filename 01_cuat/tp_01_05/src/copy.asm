fc2ed9bac0fff06315a3bf0eb873ec0cb6b25a3e
Rodrigo Anton <ranton@est.frba.utn.edu.ar>
2019-05-15 22:46:30 -0300

    Funciona

--- 01_cuat/tp_01_05/src/copy.asm
+++ 01_cuat/tp_01_05/src/copy.asm
@@ -1,23 +1,19 @@
-EXTERN __LONGITUD_RUTINAS       ; Cuando junto todos los módulos el linker script me define el valor
-EXTERN __INICIO_ROM_RUTINAS     ; Fuente
-EXTERN __INICIO_RAM_RUTINAS     ; Destino
-
 GLOBAL copy     ; Significa que cualquiera lo puede usar
 
 section .copy     ; Instruccion para el linker
 USE32       ; Le tengo que forzar a que use 32 bits porque arranca por defecto en 16
 
   copy:
-    mov esi, __INICIO_ROM_RUTINAS
-    mov edi, __INICIO_RAM_RUTINAS
-    mov ecx, __LONGITUD_RUTINAS
+    mov ebp, esp
+    mov ecx, [ebp + 4]
+    mov edi, [ebp + 8]
+    mov esi, [ebp + 12]
 
   ciclo:
     mov al, [esi]     ; mov solo copia de memoria a registro y viceversa
     mov [edi], al     ; entonces tengo que traerlo a registro y despues guardarlo
     inc esi           ; Incremento los punteros
     inc edi
-    dec ecx
-    jne ciclo
-    ; Todo esto puede ser reemplazado por "CLD" y "REP MOVSB".
+    dec ecx           ; Cuando llega a cero me levanta el flag de zero
+    jne ciclo         ; El jne se fija en el flag de zero
     ret
