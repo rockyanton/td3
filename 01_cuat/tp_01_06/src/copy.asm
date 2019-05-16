0e253b6de2f20ff4d69a1f65ac57ddbe331934a0
ranton <ranton@est.frba.utn.edu.ar>
2019-05-16 13:46:08 -0300

    Copiados los archivos del TP1.5 al TP1.6

--- 01_cuat/tp_01_06/src/copy.asm
+++ 01_cuat/tp_01_06/src/copy.asm
@@ -1,19 +1,29 @@
-GLOBAL copy     ; Significa que cualquiera lo puede usar
+;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;+++++++++++++++++++++++++++++++ DEFINES +++++++++++++++++++++++++++++++++++++
+;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+%define breakpoint  xchg bx,bx
 
-section .copy     ; Instruccion para el linker
+;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;+++++++++++++++++++++++++++++ RUTINA COPY +++++++++++++++++++++++++++++++++++
+;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;--------- Parámetros globales ------------
+GLOBAL copy     ; Para poder usar esa etiqueta en otro archivo
+section .copy
 USE32       ; Le tengo que forzar a que use 32 bits porque arranca por defecto en 16
 
-  copy:
-    mov ebp, esp
-    mov ecx, [ebp + 4]
-    mov edi, [ebp + 8]
-    mov esi, [ebp + 12]
+  ;--------- Levanto los valores de pila ------------
+  copy:                 ; Como es una pila, los saco en el orden inverso a como los cargué
+    mov ebp, esp        ; Copio el pintero a la pila, para no usarlo directamente
+    mov ecx, [ebp + 4]  ; Copio el LARGO
+    mov edi, [ebp + 8]  ; Copio el DESTINO
+    mov esi, [ebp + 12] ; Copio el ORIGEN
 
+  ;--------- Loop para copia ------------
   ciclo:
-    mov al, [esi]     ; mov solo copia de memoria a registro y viceversa
-    mov [edi], al     ; entonces tengo que traerlo a registro y despues guardarlo
+    mov al, [esi]     ; La intrucciíon "mov" solo copia de memoria a registro y viceversa.
+    mov [edi], al     ; Entonces tengo que traerlo a registro y despues guardarlo
     inc esi           ; Incremento los punteros
     inc edi
-    dec ecx           ; Cuando llega a cero me levanta el flag de zero
+    dec ecx           ; Decremento el largo. Cuando llega a cero me levanta el flag de zero
     jne ciclo         ; El jne se fija en el flag de zero
     ret
