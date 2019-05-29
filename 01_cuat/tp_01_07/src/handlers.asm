cbf66ec222ce85f50a4a6127f359b2608d7f9c58
ranton <ranton@est.frba.utn.edu.ar>
2019-05-29 16:13:44 -0300

    Se agrego la rutina que carga los handlers en la idt

---
+++ 01_cuat/tp_01_07/src/handlers.asm
@@ -0,0 +1,13 @@
+section .handlers
+
+    handler_de:
+      iret
+
+    handler_ud:
+      iret
+
+    handler_df:
+      iret
+
+    handler_gp:
+      iret
--- 01_cuat/tp_01_07/src/system_tables.asm
+++ 01_cuat/tp_01_07/src/system_tables.asm
@@ -33,8 +33,80 @@ section .tablas_de_sistema nobits
      dw long_idt-1
      dd idt
 
-     ;dw handler_timer
-     ;dw cs_sel
-     ;db 0x0
-     ;db 0x8E
-     ;dw 0xFFFF
+section .init
+    init_idt:
+      ; Excepcion de división por cero (DE), codigo 0
+      push handler_de
+      push 0x00
+      call load_handler_idt
+      pop eax
+      pop eax
+
+      ; Excepcion de Opcode inválido (UD), codigo 6
+      push handler_ud
+      push 0x06
+      call load_handler_idt
+      pop eax
+      pop eax
+
+      ; Excepcion de Doble Falta (DF), codigo 8
+      push handler_df
+      push 0x08
+      call load_handler_idt
+      pop eax
+      pop eax
+
+      ; Excepcion de General Protection (GP), codigo 13
+      push handler_gp
+      push 0x0D
+      call load_handler_idt
+      pop eax
+      pop eax
+
+    ret
+
+
+    EXTERN cs_sel_prim
+
+    load_handler_idt:
+      mov esi, idt
+      mov ebp, esp        ; Copio el puntero a la pila, para no usarlo directamente
+      mov ecx, [ebp + 4]  ; Numero de excepción / interrupción
+      mov edi, [ebp + 8]  ; Dirección del handler
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 0 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Bits 0-7 del Offset
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 1 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Bits 8-15 del Offset
+      mov [esi + ecx*8],si
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 2 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Selector se segmento (primera parte)
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 3 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Selector se segmento (segunda parte)
+      mov [esi + ecx*8 +2],cs_sel_prim
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 4 del descriptor IDT
+      ;   | | | `-`-`-`-`---- Reservado
+      ;   `-`-`------------- "000"
+      mov [esi + ecx*8 +4],0x0
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 5 del descriptor IDT
+      ;   | | | | | `-`-`---- "110"
+      ;   | | | | `--------- D (Size of gate): 1=32bits   0=16bits
+      ;   | | | `---------- "0"
+      ;   | `-`----------- DPL (Descriptor Privilege Level): 0=SO ... 3:User
+      ;   `-------------- P (Segment Present Flag)
+      mov [esi + ecx*8 +4],0x8E   ;0x8E = 1 00 0 1 110 ==> Segmento Presente, Permisos elevados, tamaño del gate de 32bits
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 6 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Bits 16-23 del Offset
+
+      ;  |7|6|5|4|3|2|1|0|  Bit 7 del descriptor IDT
+      ;   `-`-`-`-`-`-`-`---- Bits 24-31 del Offset
+      rol esi,16    ; Lo roto para obtener la parte alta
+      mov [esi + ecx*8 +6],si
+
+      ret
