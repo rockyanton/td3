5414c204a5c08cf6cf06bef992055b8f7473ac0f
ranton <ranton@est.frba.utn.edu.ar>
2019-05-31 16:09:23 -0300

    Agergada la función para guardar la excepcion

--- 01_cuat/tp_01_07/src/handlers.asm
+++ 01_cuat/tp_01_07/src/handlers.asm
@@ -1,6 +1,10 @@
 section .handlers
 
     handler_de:
+      pushad
+      push dw 0x0
+      call save_exception
+      popad
       iret
 
     handler_ud:
@@ -11,3 +15,7 @@ section .handlers
 
     handler_gp:
       iret
+
+    save_exception:
+      mov ebp, esp        ; Copio el pintero a la pila, para no usarlo directamente
+      mov ecx, [ebp + 4]  ; Saco el número de excepcion
